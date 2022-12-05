617116cbb73ca4800bfe7f0f4108c29673a8b516
qordjs <94037049+qordjs@users.noreply.github.com>
2022-12-06 04:10:27 +0900

    Add files via upload

---
+++ Login2.php
@@ -0,0 +1,36 @@
+<?php
+    $con = mysqli_connect("localhost", "baekun", "1234", "test");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $userID = $_POST["userID"];
+    $userPassword = $_POST["userPassword"];
+    
+    
+    $statement = mysqli_prepare($con, "SELECT * FROM USER WHERE userID = ? AND userPassword = ?");
+    mysqli_stmt_bind_param($statement, "ss", $userID, $userPassword);
+    mysqli_stmt_execute($statement);
+
+
+    mysqli_stmt_store_result($statement);
+    mysqli_stmt_bind_result($statement, $userID, $userPassword, $userName, $userAge, $userHak, $userMajor);
+
+    $response = array();
+    $response["success"] = false;
+    
+    while(mysqli_stmt_fetch($statement)) {
+        $response["success"] = true;
+        $response["userID"] = $userID;
+        $response["userPassword"] = $userPassword;
+        $response["userName"] = $userName;
+        $response["userAge"] = $userAge;
+        $response["userHak"] =
+        $userHak;
+        $response["userMajor"] =
+        $userMajor;
+    }
+
+    echo json_encode($response);
+
+
+
+?>
---
+++ Register2.php
@@ -0,0 +1,25 @@
+<?php 
+    $con = mysqli_connect("localhost", "baekun", "1234", "test");
+    mysqli_query($con,'SET NAMES utf8');
+
+    $userID = $_POST["userID"];
+    $userPassword = $_POST["userPassword"];
+    $userName = $_POST["userName"];
+    $userAge = $_POST["userAge"];
+    $userHak = $_POST["userHak"];
+    $userMajor = $_POST["userMajor"];
+
+    $statement = mysqli_prepare($con, "INSERT INTO USER VALUES (?,?,?,?,?,?)");
+    mysqli_stmt_bind_param($statement, "sssiis", $userID, $userPassword, $userName, $userAge, $userHak, $userMajor);
+    mysqli_stmt_execute($statement);
+
+
+    $response = array();
+    $response["success"] = true;
+ 
+   
+    echo json_encode($response);
+
+
+
+?>
